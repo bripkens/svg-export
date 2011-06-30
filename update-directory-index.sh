6c5d5a9a549a01d78a82a809a4d8a9506122eeaf
Ben Ripkens <bripkens.dev@gmail.com>
2011-06-30 09:44:00 +0200

    Fixed an error in the generation of the HTML files.

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -2,8 +2,8 @@
  
 for DIR in $(find ./repository -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    echo "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo -e "</pre>\n</body>\n</html>"
+    echo "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
